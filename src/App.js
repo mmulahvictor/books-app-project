3411e83ccb2d3e3b1407ff8af637ecf3ff7a32c9
mmulahvictor <mmulahvictor@gmail.com>
2022-07-27 02:17:53 +0300

    create card and book component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Book from './components/Book';
 
 function App() {
   return (
     <>
+      <Book />
     </>
   );
 }
---
+++ src/components/Book.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+
+function Book () {
+    return (
+        <>
+            <div>
+                
+            </div>
+            <div>
+                { <Card /> }
+            </div>
+        </>
+    );
+}
+
+export default Book;
