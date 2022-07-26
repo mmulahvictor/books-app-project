5f9ce7366d8987b7723c62460562013b984b60c2
mmulahvictor <mmulahvictor@gmail.com>
2022-07-27 02:58:48 +0300

    added images, hearder and input/search form

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,8 +4,18 @@ import Card from './Card';
 function Book () {
     return (
         <>
-            <div>
-                
+            <div className='header'>
+                <div className='row'>
+                    <h1>Good friends, good books, and a sleepy conscience: <br/> this is the ideal life.</h1>
+                </div>
+                <div className="row1">
+                    <h2>Search for a book</h2>
+                    <div className="find">
+                        <input type="text" placeholder='Enter the Book name' />
+                        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                    <img src="" alt="" />
+                </div>
             </div>
             <div>
                 { <Card /> }
