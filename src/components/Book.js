1b15e62e3e9f38cb04b7eaaba6f9e15c62dc39a1
mmulahvictor <mmulahvictor@gmail.com>
2022-07-27 22:44:32 +0300

    import data from an external API

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './Card';
+import axios from "axios"
 
-function Book () {
+const Book = () => {
+    const [ search, setSearch ] = useState( "" );
+    const [ bookData, setData ] = useState( [] );
+    const searchBook = ( event ) => {
+        if ( event.key === "Enter" )
+        {
+            // eslint-disable-next-line no-useless-concat
+            axios.get( 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU' + '&maxResults=40' )
+                .then( res => setData( res.data.items ) )
+                .catch( err => console.log( err ) );
+        }
+    }
     return (
         <>
             <div className='header'>
@@ -11,14 +23,14 @@ function Book () {
                 <div className="row1">
                     <h2>Search for a book</h2>
                     <div className="find">
-                        <input type="text" placeholder='Enter the Book name' />
-                        <button><i className="fa-solid fa-magnifying-glass"></i></button>
+                        <input type="text" placeholder='Enter the Book name' value={search} onChange={e=>setSearch(e.target.value)} onKeyPress={searchBook} />
+                        <button><i className="fas fa-search"></i></button>
                     </div>
-                    <img src="../images/download (1).jpeg" alt="" />
+                    <img src="./images/download (1).jpeg" alt="" />
                 </div>
             </div>
-            <div>
-                { <Card /> }
+            <div className='container'>
+                { <Card book={bookData} /> }
             </div>
         </>
     );
