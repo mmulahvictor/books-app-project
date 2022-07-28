330b2be6f0d51f4448f79e85b9e4de19e87cc9bc
mmulahvictor <mmulahvictor@gmail.com>
2022-07-28 18:02:31 +0300

    minor changes

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,21 +3,22 @@ import axios from 'axios';
 
 const Book = () => {
     const [ searchItem, setSearchItem ] = useState( "" );
-    const [books, setBooks]=useState({items: []})
-    const onInputChange = ( e ) => {
+    const [ books, setBooks ] = useState( { items: [] } );
+    const onInputChange = e => {
         setSearchItem( e.target.value );
     };
 
-    const url = `https://www.googleapis.com/books/v1/volumes`;
+    let url = `https://www.googleapis.com/books/v1/volumes`;
+
     const fetchBooks = async () => {
         const result = await axios.get( `${ url }?q=${ searchItem }` );
         setBooks( result.data );
-    }
+    };
 
-    function onSubmitHandle ( e ) {
-        e.preventDefault()
-        fetchBooks()
-    }
+    const onSubmitHandler = e => {
+        e.preventDefault();
+        fetchBooks();
+    };
 
     const bookAuthors = authors => {
         if ( authors.length <= 2 )
@@ -35,30 +36,38 @@ const Book = () => {
 
     return (
         <section>
-            <form onSubmit={ onSubmitHandle }>
+            <form onSubmit={ onSubmitHandler }>
                 <label>
                     <span>Search for books</span>
-                    <input type="search" placeholder="Search for your book" value={ searchItem } onChange={onInputChange} />
+                    <input
+                        type="search"
+                        placeholder="Search for your book"
+                        value={ searchItem }
+                        onChange={ onInputChange }
+                    />
                     <button type="submit">Search</button>
                 </label>
             </form>
             <ul>
-                {
-                    books.items.map( ( book, index ) => {
-                        return (
-                            <li key={index}>
+                { books.items.map( ( book, index ) => {
+                    return (
+                        <li key={ index }>
+                            <div>
+                                <img
+                                    alt={ `${ book.volumeInfo.title } book` }
+                                    src={ `http://books.google.com/books/content?id=${ book.id
+                                        }&printsec=frontcover&img=1&zoom=1&source=gbs_api` }
+                                />
                                 <div>
-                                    <img src="{`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}" alt="{`${book.volumeInfo.title} book`}" />
-                                    <div>
-                                        <h3>{ book.volumeInfo.title }</h3>
-                                        <p>{ bookAuthors( book.volumeInfo.authors ) }</p>
-                                        <p>{ book.volumeInfo.publishedDate }</p>
-                                    </div>
-                                </div><hr />
-                            </li>
-                        )
-                    })
-                }
+                                    <h3>{ book.volumeInfo.title }</h3>
+                                    <p>{ bookAuthors( book.volumeInfo.authors ) }</p>
+                                    <p>{ book.volumeInfo.publishedDate }</p>
+                                </div>
+                            </div>
+                            <hr />
+                        </li>
+                    );
+                } ) }
             </ul>
         </section>
     );
