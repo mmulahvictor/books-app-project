e840e6b89f7d32c46976d8a42755d7c5e7eee163
mmulahvictor <mmulahvictor@gmail.com>
2022-07-28 17:01:31 +0300

    add form for searching books

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,39 +1,17 @@
-import React, {useState} from 'react';
-import Card from './Card';
-import axios from "axios"
+import React from 'react';
 
 const Book = () => {
-    const [ search, setSearch ] = useState( "" );
-    const [ bookData, setData ] = useState( [] );
-    const searchBook = ( event ) => {
-        if ( event.key === "Enter" )
-        {
-            // eslint-disable-next-line no-useless-concat
-            axios.get( 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU' + '&maxResults=40' )
-                .then( res => setData( res.data.items ) )
-                .catch( err => console.log( err ) );
-        }
-    }
     return (
-        <>
-            <div className='header'>
-                <div className='row'>
-                    <h1>Good friends, good books, and a sleepy conscience: <br/> this is the ideal life.</h1>
-                </div>
-                <div className="row1">
-                    <h2>Search for a book</h2>
-                    <div className="find">
-                        <input type="text" placeholder='Enter the Book name' value={search} onChange={e=>setSearch(e.target.value)} onKeyPress={searchBook} />
-                        <button><i className="fas fa-search"></i></button>
-                    </div>
-                    <img src="./images/download (1).jpeg" alt="" />
-                </div>
-            </div>
-            <div className='container'>
-                { <Card book={bookData} /> }
-            </div>
-        </>
+        <section>
+            <form>
+                <label>
+                    <span>Search for books</span>
+                    <input type="search" placeholder="Search for your book.,"/>
+                    <button type="submit">Search</button>
+                </label>
+            </form>
+        </section>
     );
-}
+};
 
 export default Book;
