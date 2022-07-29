b8dd7afb0050b48e7620c346cc0eda33ff7aa22a
mmulahvictor <mmulahvictor@gmail.com>
2022-07-29 09:26:12 +0300

    minor change on about component

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import './Style.css'
 function About() {
   return (
       <div className='about'>
-          <h1>Some motivation</h1>
+          <h1>Some information?</h1>
           <p>
               “The best programs are written so that computing machines can perform them quickly and so that human beings can understand them clearly. A programmer is ideally an essayist who works with traditional aesthetic and literary forms as well as mathematical concepts, to communicate the way that an algorithm works and to convince a reader that the results will be correct.”
               <strong>― Donald E. Knuth, Selected Papers on Computer Science</strong>
